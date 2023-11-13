0692d3178714b631e6059232bf069d5b762e79c4
KhanhHoang <nguyenlehoangkhanh1679@gmail.com>
2023-11-13 19:50:22 +0700

    bear token

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -12,7 +12,7 @@ const createJWT = (payload) => {
     let token = null;
 
     try {
-        token = jwt.sign(payload, secretKey);
+        token = jwt.sign(payload, secretKey, {expiresIn: process.env.JWT_EXPIRESIN});
     } catch (error) {
         console.log('error create token: ', error)
     }
@@ -37,6 +37,13 @@ const verifyToken = (token) => {
     return data;
 }
 
+const extractToken = (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } 
+    return null;
+}
+
 const checkUserJWT = (req, res, next) => {
     
     if(nonSecurePaths.includes(req.path)){
@@ -44,10 +51,11 @@ const checkUserJWT = (req, res, next) => {
     }
 
     let cookies = req.cookies;
+    const tokenFromHeader = extractToken(req);
 
-    if(cookies && cookies.jwt){
+    if((cookies && cookies.jwt) || tokenFromHeader){
 
-        let token = cookies.jwt
+        let token = cookies && cookies.jwt ? cookies.jwt : tokenFromHeader
         let decoded = verifyToken(token);
 
         if(decoded) {
@@ -60,7 +68,8 @@ const checkUserJWT = (req, res, next) => {
                 errorCode: -1, // Error code
             });
         }
-    }else{
+    }
+    else{
         console.log('khong co cookies ')
         return res.status(401).json({
             message: "Not authenticated the user", //Error message
